1f42f62bc5acb5e1a67890b1724b41be32502b79
fester3000@gmail.com <fester3000@gmail.com@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-04-24 15:53:24 +0000

    NetworkNode project add

---
+++ NetworkNode/NetworkNode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkNode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            System.Console.WriteLine("You entered the following {0} command line arguments: ", args.Length );
+            for (int i = 0; i < args.Length; i++)
+            {
+                System.Console.WriteLine("{0}", args[i]);
+            }
+            System.Console.ReadKey();
+            return 0;
+        }
+    }
+}
