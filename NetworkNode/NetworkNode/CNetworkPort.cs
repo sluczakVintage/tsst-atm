c408519883bbdf5fafbf084f60487f16f515875c
adam.zadura <adam.zadura@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-04-25 12:59:38 +0000

    Dodane modele portów w węźle sieciowym. Ręcznie dodany model ClientNode do Solution. Co do portów, czy nie powinny one mieć jeszcze bufora?

--- NetworkNode/NetworkNode/CNetworkPort.cs
+++ NetworkNode/NetworkNode/CNetworkPort.cs
@@ -7,5 +7,33 @@ namespace NetworkNode
 {
     class CNetworkPort
     {
+        private int _id;
+        private Boolean _busy;
+
+        public CNetworkPort(int id, Boolean busy)
+        {
+            this._id = id;
+            this._busy = busy;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public Boolean STATUS
+        {
+            get
+            {
+                return _busy;
+            }
+            set
+            {
+                _busy = value;
+            }
+        }
     }
 }
