4838fe4f6a42d6fb889f339fb0f0f8888ee6a3c1
fester3000@gmail.com <fester3000@gmail.com@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-04-25 10:58:14 +0000

    byte[] array swapped with List<byte>

--- ClientNode/ClientNode/CUserData.cs
+++ ClientNode/ClientNode/CUserData.cs
@@ -7,6 +7,22 @@ namespace Data
 {
     class CUserData
     {
-        private fixed byte information[48];
+        private static int MAX_CAPACITY = 48;
+        private List<byte> data;
+
+        CUserData()
+        {
+            data = new List<byte>();
+        }
+
+        public void setInformation(List<byte> data)
+        {
+            if(data.Capacity <= MAX_CAPACITY)
+                this.data = data;
+        }
+        public List<byte> getInformation()
+        {
+            return data;
+        }
     }
 }
