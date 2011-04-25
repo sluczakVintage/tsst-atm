bab08c027d9b2c0239e2f2b51d7e360f39a4a097
adam.zadura <adam.zadura@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-04-25 13:03:13 +0000

    Tym razem dodane wszystko o czym mowa w poprzednim commicie.

---
+++ NetworkNode/NetworkNode/CNetworkPortIn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkNode
+{
+    class CNetworkPortIn : CNetworkPort
+    {
+        public CNetworkPortIn(int id, Boolean status):base(id, status)
+        {}
+
+        public void receiveData() //metoda odbierajace dane 
+        { }
+    
+    } 
+}
---
+++ NetworkNode/NetworkNode/CNetworkPortOut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkNode
+{
+    class CNetworkPortOut:CNetworkPort
+    {
+        private int _id;
+        private Boolean _busy;
+
+        public CNetworkPortOut(int id, Boolean busy):base(id, busy)    
+        {}
+
+        public void send()
+        { }
+    }
+}
