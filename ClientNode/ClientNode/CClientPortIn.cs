0c28bfc02c825057385cc5916193dcfe23da555b
maku17 <maku17@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-04-26 23:16:53 +0000

    dodałem sockety

--- ClientNode/ClientNode/CClientPortIn.cs
+++ ClientNode/ClientNode/CClientPortIn.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 // Klasa portu wyjściowego dziedzicząca po CClientPort
 
@@ -9,12 +12,48 @@ namespace ClientNode
 {
     class CClientPortIn : CClientPort
     {
-
+        private bool status;
+        private IPAddress ip = IPAddress.Parse("127.0.0.1");     //adres serwera
+        private TcpListener portListener;
+        private TcpClient client;
+        private NetworkStream clientStream;
+        private StreamWriter serwerStream;
+        private static String helloMessage = "Welcome to port : " ;
+        
         public CClientPortIn(int i, Boolean p):       
            base(i, p){}
+
+
+        public void init(int portNumber) //metoda uruchamiająca nasłuchiwanie na porcie. 
+        {
+            status = true;
+            portListener = new TcpListener(ip, portNumber);  //tworzymy obiekt  nasłuchujący na podanym porcie
+            portListener.Start();                      //uruchamiamy serwer
+
+            client = portListener.AcceptTcpClient(); //akceptujemy żądanie połączenia
+            clientStream = client.GetStream();  //pobieramy strumień do wymiany danych
+            Console.WriteLine("connection accepted ");
+            while (status) //uruchamiamy nasłuchiwanie
+            {
+                StreamReader sr = new StreamReader(clientStream);
+                String dane = sr.ReadLine();
+                Console.WriteLine(dane);
+            }
+
+        }
+
+        public void shutdown()
+        {
+            status = false;
+            client.Close();
+            portListener.Stop();
+        }
+
+
+    
         
 
-
+    
     }
 }
 
--- ClientNode/ClientNode/CClientPortOut.cs
+++ ClientNode/ClientNode/CClientPortOut.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace ClientNode
 {
@@ -9,20 +12,43 @@ namespace ClientNode
 
     class CClientPortOut : CClientPort
     {
-        private int i;
-        private bool p;
 
+        private int _id;
+        private bool _busy;
+        private static TcpClient client;
+        private static String ip = "127.0.0.1";
+        private NetworkStream stream;
+        private StreamWriter clientStream;
+
+        //konstruktor
         public CClientPortOut(int i, bool p): base(i,p){}
+
+        public void init(int portNumber) // metoda łącząca socket z portNumber
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ip, portNumber);
+                stream = client.GetStream();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error.... " + e.StackTrace);
+            }
+        }
+
+        public void shutdown()
+        {
+            client.Close();
+        }
 
         // metoda nawiązująca połączenie z węzłem sieciowym i nadająca do niego TO DO
         public void send(String str)
         {
             Console.WriteLine("nadaje " + str);
-        }
-
-        public void stop()
-        {
-            Console.WriteLine("wstrzymałem nadawnie");
+            clientStream = new StreamWriter(stream);
+            clientStream.WriteLine(str);
+            clientStream.Flush();
         }
     }
 }
