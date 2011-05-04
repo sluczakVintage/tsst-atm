c7eacfe696effa8b2647ad346623b7e3623d6cd3
adam.zadura@gmail.com <adam.zadura@gmail.com@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-05-04 22:05:04 +0000

    dodana klasa CSNMPmessage.cs

---
+++ Data/CSNMPmessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Data
+{
+    public class CSNMPmessage
+    {
+        private string version;
+        private string community;
+        private SNMPpdu pdu;
+    }
+
+    class SNMPpdu
+    {
+        private string type;
+        private List<Dictionary<String, String>> variablebinding; //lista zawierajaca jedna lub wiecej par nazwa obiektu - wartosci, dalsze tlumaczenie tego w ksiazce jest dla mnie lekko niezrozumiale o tej porze...
+        private string requestIdentifier;
+    }
+    
+
+    class SNMPpduResponse : SNMPpdu
+    {
+        private int errorStatus;           // w SNMPpdu dla request na tych miejscach sa 0 wiec nie definiowalem nowej klasy dla niego
+        private int errorPosition; 
+    
+    }
+
+   // class SNMPpduTrap :SNMPpdu  chyba nie bedziemy tego uzywac
+   // {}
+
+
+
+}
