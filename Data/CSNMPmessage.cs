250156fe79cd9484fb7d9f2e1a429415258b3ebf
adam.zadura@gmail.com <adam.zadura@gmail.com@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-05-04 22:26:31 +0000

    dodane akcesory do pól prywatnych

--- Data/CSNMPmessage.cs
+++ Data/CSNMPmessage.cs
@@ -11,6 +11,42 @@ namespace Data
         private string version;
         private string community;
         private SNMPpdu pdu;
+
+        string Version
+        {
+            get
+            {
+                return version;
+            }
+            set
+            {
+                version = value;
+            }
+        }
+
+        string Community
+        {
+            get
+            {
+                return community;
+            }
+            set
+            {
+                community = value;
+            }
+        }
+
+        SNMPpdu PDU
+        {
+            get
+            {
+                return pdu;
+            }
+            set
+            {
+                pdu = value;
+            }
+        }
     }
 
     class SNMPpdu
@@ -18,14 +54,64 @@ namespace Data
         private string type;
         private List<Dictionary<String, String>> variablebinding; //lista zawierajaca jedna lub wiecej par nazwa obiektu - wartosci, dalsze tlumaczenie tego w ksiazce jest dla mnie lekko niezrozumiale o tej porze...
         private string requestIdentifier;
+
+        string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        string RequestIdentifier
+        {
+            get
+            {
+                return requestIdentifier;
+            }
+            set
+            {
+                requestIdentifier = value;
+            }
+        }
+
+
     }
     
 
     class SNMPpduResponse : SNMPpdu
     {
         private int errorStatus;           // w SNMPpdu dla request na tych miejscach sa 0 wiec nie definiowalem nowej klasy dla niego
-        private int errorPosition; 
-    
+        private int errorPosition;
+
+        int ErrorStatus
+        {
+            get
+            {
+                return errorStatus;
+            }
+            set
+            {
+                errorStatus = value;
+            }
+        }
+
+        int ErrorPosition
+        {
+            get
+            {
+                return errorPosition;
+            }
+            set
+            {
+                errorPosition = value;
+            }
+        }
+
     }
 
    // class SNMPpduTrap :SNMPpdu  chyba nie bedziemy tego uzywac
