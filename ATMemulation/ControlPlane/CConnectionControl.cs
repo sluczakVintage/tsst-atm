354019464e57630e3c1e65fd62e786f0bc42a91e
adam.zadura@gmail.com <adam.zadura@gmail.com@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-06-05 15:37:37 +0000

    dodany 'interfejs' w CConnectionControl.cs

---
+++ ATMemulation/ControlPlane/CConnectionControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ControlPlane
+{
+    class CConnectionControl
+    {
+        //metoda do wymiany pomiedzy CC
+        // parametry: para SNP, SNP i SNPP, para SNPP
+        // zwraca: potwierdzenie
+        public bool PeerCoordinationOut(int SNP_s, int SNP_d)
+        {
+            bool confirmation=false;
+            return confirmation;
+        }
+        //metoda zadajaca zestawienia polaczenia. uzywana w trybie hierarchicznym
+        //parametry: para SNP
+        //zwraca: polaczenie podsieciowe
+        public void ConnectionRequestOut(int SNP_s, int SNP_d)
+        {}
+        
+
+        //metoda kierowana do RC by uzyskac sciezke pomiedzy dwoma punktami 
+        //parametry: 'unresolved route fragment'
+        //zwraca: zbior SNPP
+        public void RouteTableQuery()
+        {}
+
+        //metoda do zestawienia polaczenia? kierowana do LRM
+        //parametry:brak
+        //zwraca: link connection ( pare SNP)
+        public void LinkConnectionRequest()
+        {
+        }
+
+        public void LinkConnectionDeallocation(int SNP)
+        { }
+    }
+}
---
+++ ClientNode/ClientNode/CPCC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientNode
+{
+    class CPCC
+    {
+        public void CallRequest()
+        {}
+        public void CallTeardown()
+        {}
+
+    }
+}
