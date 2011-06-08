3c701c60860f9d6de1f69836c705806d4b05f8b9
fester3000@gmail.com <fester3000@gmail.com@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-06-08 09:39:56 +0000

    Starter

---
+++ ATMemulation/starter/start.bat
@@ -0,0 +1,10 @@
+#start ManagementLayer.exe.lnk
+sleep -m 1000
+#start ControlPlane.exe.lnk
+sleep -m 1000
+start NetworkNode.exe.lnk 5
+start NetworkNode.exe.lnk 3
+start ClientNode.exe.lnk 1
+start ClientNode.exe.lnk 4
+
+
