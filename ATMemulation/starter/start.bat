4f36c081df617c5580bc031b944c9f1eb6c427cc
maku17 <maku17@9aaeae3e-551f-2a2a-6a41-840efb5d5908>
2011-06-09 16:06:43 +0000

    przeniesiony networkConfig.xml do startera

--- ATMemulation/starter/start.bat
+++ ATMemulation/starter/start.bat
@@ -2,7 +2,7 @@
 sleep -m 1000
 #start ControlPlane.exe.lnk
 sleep -m 1000
-start NetworkNode.exe.lnk 5
+#start NetworkNode.exe.lnk 5
 start NetworkNode.exe.lnk 3
 start NetworkNode.exe.lnk 2
 #start ClientNode.exe.lnk 1
